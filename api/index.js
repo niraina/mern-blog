152c25823ae996c73fd0400464822b05119ba939
Niraina <stg-eni-niraina@livenexx.net>
2024-01-19 14:47:11 +0300

    Header + server

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("MongoDB is connected")
+    })
+
+const app = express();
+
+app.listen(3500, () => {
+    console.log('Server is running on port 3500')
+})
